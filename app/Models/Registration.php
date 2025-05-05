47cf640f831a6778d56d18bf066c23815ed8724d
dawnStreaks <sabeehakpm@gmail.com>
2025-05-05 10:10:35 +0300

    changes to university_events

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -10,5 +10,6 @@ class Registration extends Model
     public function event() {
         return $this->belongsTo(Event::class);
     }
-    
+    protected $fillable = ['event_id', 'name', 'email'];
+
 }
